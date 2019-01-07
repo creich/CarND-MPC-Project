18aeb4bddc3aef1d76c07dc23a1bf067471098db
creich <creich@linux.com>
2019-01-07 14:10:08 +0100

    first draft... weird runtime error

--- src/main.cpp
+++ src/main.cpp
@@ -85,25 +85,66 @@ int main() {
         std::string event = j[0].get<std::string>();
         if (event == "telemetry") {
           // j[1] is the data JSON object
+          // 6 waypoints from the simulator
           std::vector<double> ptsx = j[1]["ptsx"];
           std::vector<double> ptsy = j[1]["ptsy"];
+          // current car data
           double px = j[1]["x"];
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
 
+          // artificially fix the cars reference system in a way that
+          //   * the car's angle psi will be 0
+          //   * the 'location' is set to origin
+          // just to simplify the math (e.g. for polyfit)
+          for(int i=0; i<ptsx.size(); i++) {
+             double shift_x = ptsx[i] - px;
+             double shift_y = ptsy[i] - py;
+
+             ptsx[i] = shift_x * cos(0-psi) - shift_y * sin(0-psi);
+             ptsy[i] = shift_x * sin(0-psi) + shift_y * cos(0-psi);
+          }
+
+          // put waypoints into Eigen::vectors to feed them to polyfit
+          // found a way to transform the vectors easily here: 
+          //   https://forum.kde.org/viewtopic.php?f=74&t=94839
+          //   https://eigen.tuxfamily.org/dox/classEigen_1_1Map.html
+          //   Eigen::VectorXd ptsx_eigen = Eigen::VectorXd::Map(ptsx.data(), ptsx.size());
+          Eigen::Map<Eigen::VectorXd> ptsx_eigen(ptsx.data(), ptsx.size());
+          Eigen::Map<Eigen::VectorXd> ptsy_eigen(ptsy.data(), ptsy.size());
+
+          Eigen::VectorXd coeffs = polyfit(ptsx_eigen, ptsy_eigen, 3);
+
+          // calculate CTE and ePSI
+          double cte = polyeval(coeffs, 0);
+          //TODO check if we wanne use the full implementation of ePSI
+          // correct implementation of ePSI:
+          //double epsi = psi - atan( coeffs[1] + 2 * px * coeffs[2] + 3 * coeffs[3] * pow(px, 2) );
+          // can be simplified due to our reference system simplification that made px == 0 and psi == 0 to
+          // epsi = 0 - atan( coeffs[1] + 2 * 0 * coeffs[2] + 3 * coeffs[3] * pow(0, 2))
+          // epsi = 0 - atan( coeffs[1] +         0         +          0               )
+          double epsi = -atan(coeffs[1]);
+
+          Eigen::VectorXd state(6);
+          state << 0, 0, 0, v, cte, epsi;
+
           /*
-          * TODO: Calculate steering angle and throttle using MPC.
+          * Calculate steering angle and throttle using MPC.
           *
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
 
-          json msgJson;
+          std::vector<double> vars = mpc.Solve(state, coeffs);
+
+          double Lf = 2.67;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          double steer_value = vars[0] / (deg2rad(25)*Lf);
+          double throttle_value = vars[1];
+
+          json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
@@ -114,15 +155,32 @@ int main() {
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
 
+          for(int i=0; i<vars.size(); i++) {
+             if(i%2 == 0) {
+                mpc_x_vals.push_back(vars[i]);
+             } else {
+                mpc_y_vals.push_back(vars[i]);
+             }
+          }
+
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
           //Display the waypoints/reference line
+          // for better debugging we calc the line that the car will try to
+          // follow and make it displayable within the simulator
           std::vector<double> next_x_vals;
           std::vector<double> next_y_vals;
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
+          double poly_inc = 2.0;
+          int num_way_points = 23;
+          for(int i=0; i<num_way_points; i++) {
+             next_x_vals.push_back(poly_inc * i);
+             next_y_vals.push_back(polyeval(coeffs, poly_inc * i));
+          }
+
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
